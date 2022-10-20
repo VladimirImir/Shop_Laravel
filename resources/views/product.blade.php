b2335f0b1a514247fb2b8f6366a0f7d7ed2f5df5
VladimirImir <mimimvvv@gmail.com>
2022-10-20 15:50:15 +0300

    step 5

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,15 +4,11 @@
 
 @section('content')
     <div class="starter-template">
-        <h1>iPhone X 64GB</h1>
-        <h2>{{$product}}</h2>
-        <h2>Мобильные телефоны</h2>
-        <p>Цена: <b>71990 ₽</b></p>
-        <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg">
+        <h2>{{ $product }}</h2>
+        <p>Цена: <b>71990 руб.</b></p>
+        <img src="http://laravel-diplom-1.rdavydov.ru/storage/products/iphone_x.jpg">
         <p>Отличный продвинутый телефон с памятью на 64 gb</p>
-        <form action="http://internet-shop.tmweb.ru/basket/add/1" method="POST">
-            <button type="submit" class="btn btn-success" role="button">Добавить в корзину</button>
-            <input type="hidden" name="_token" value="0FKS3B0xZRHQxrFj0AumlmPn2dN1VrpyHW4vfL2y">        </form>
+        <a class="btn btn-success" href="http://laravel-diplom-1.rdavydov.ru/basket/1/add">Добавить в корзину</a>
     </div>
 @endsection
 
